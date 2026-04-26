7bec1ffb1a443bdc21cc6627920f9dddeb626f03
JJ League Stats <276931665+JJLStats@users.noreply.github.com>
2026-04-26 22:30:44 +0200

    Add League 38

---
+++ league_38/data/club_datas.js
@@ -0,0 +1,11 @@
+const WeekID = [
+  "W1"
+]
+const ClubID = {
+  "th": "The Hangover",
+  "ie": "Imperial Evidence",
+  "cs": "Competition Seekers",
+  "sa": "SECRET ALLIANCE",
+  "ka": "Kicking Ass",
+  "dd": "Dragonfly Detective Agency"
+}
